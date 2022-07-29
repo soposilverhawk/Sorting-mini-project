6f70a5427b9605c535419e363e155d5d8210e5bf
soposilverhawk <Darkness24lol@gmail.com>
2022-07-29 13:24:04 +0400

    Search-area

---
+++ main.js
@@ -0,0 +1,7 @@
+let products = [["Apple", 2.4],["Banana", 1.8], ["Cherry", 3],["Peach",1.2],["Watermelon", 0.7],["Melon", 1],["Plum", 3,2]];
+// let container = document.querySelector(".container")
+// for(i = 0; i < products.length; i++){
+//    let productsDiv = document.createElement("div");
+//    productsDiv.classList.add("products-divs")
+//    container.appendChild(productsDiv)
+// }
